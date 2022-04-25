7260439c5d7d8fc3137c2e869d94184e564bdadc
Amaro13 <amaro_ribeir0@hotmail.com>
2022-04-24 21:23:17 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,112 @@
+import express from "express"; // importando express
+import path from "path"; // serve para definir caminhos padrões
+
+const __dirname = path.resolve(path.dirname(``)); // dirname serve para informar o caminho padrão local.
+
+const app = express(); //instanciando express dentro da const app
+
+app.use(express.urlencoded({ extended: true })); // O corpo(body) da requisição
+app.use(express.json()); // Converte para JSON
+
+app.set("view engine", "ejs"); // Faz com que o express reconheça o ejs como motor de visualização
+app.use(express.static(path.join(__dirname, "public")));
+
+const port = 3001;
+
+app.listen(port, () => {
+  //listen é uma função que cria um servidor
+  console.log(`Rodando na porta ${port}`);
+});
+
+const pokedex = [
+  {
+    id: 1,
+    number: `001`,
+    name: "Bulbasaur",
+    type: "Leaf",
+    category: "Seed",
+    abilities: "Overgrow",
+    height: "0.7 m",
+    weight: "6.9 kg",
+    description:
+      "A strange seed was planted on its back at birth. The plant sprouts and grows with this pokemon.",
+    img: "/img/bulbasaur.png",
+  },
+  {
+    id: 2,
+    number: `004`,
+    name: "Charmander",
+    type: "Fire",
+    category: "Lizard",
+    abilities: "Blaze",
+    height: "0.6 m",
+    weight: "8.5 kg",
+    description:
+      "From the time it is born, a flame burns at the tip of its tail. It's life would end if its flame were to go out.",
+    img: "/img/charmander.png",
+  },
+  {
+    id: 3,
+    number: `007`,
+    name: "Squirtle",
+    type: "Water",
+    category: "Tiny Turtle",
+    abilities: "Torrent",
+    height: "0.5 m",
+    weight: "9.0 kg",
+    description:
+      "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
+    img: "/img/squirtle.png",
+  },
+];
+
+app.get(`/`, (req, res) => {
+  // get é um método http/https para criar um servidor
+  res.render("index.ejs", {
+    pokedex,
+  }); // a pasta views já é reconhecida automaticamente, logo não precisa dar o endereço completo
+});
+
+app.get(`/detalhes/:id`, (req, res) => {
+  let pokemon;
+  pokedex.filter((element) => {
+    if (element.id == req.params.id) {
+      pokemon = element;
+    }
+  });
+  console.log(pokemon);
+  res.render(`detalhes.ejs`, { pokemon });
+});
+
+app.get(`/cadastro`, (req, res) => {
+  res.render("cadastro.ejs");
+});
+
+app.post(`/cadastro`, (req, res) => {
+  let id = pokedex.length + 1;
+  const {
+    number,
+    name,
+    type,
+    category,
+    abilities,
+    height,
+    weight,
+    description,
+    img,
+  } = req.body;
+  pokedex.push({
+    id,
+    number,
+    name,
+    type,
+    category,
+    abilities,
+    height,
+    weight,
+    description,
+    img,
+  });
+  console.log(pokedex);
+  res.redirect("/");
+});
---
+++ readme.md
@@ -0,0 +1,22 @@
+# POKEDEX
+
+## DESCRIPTION
+
+Project created during the fullstack course from BlueEdtech.
+
+## STACKS
+
+The tools used during this project are:
+
+<div style="display: flex">
+    <img src="https://img.icons8.com/color/96/000000/javascript--v1.png"/>
+    <img src="https://img.icons8.com/ios-filled/100/000000/css3.png"/>
+    <img src="https://img.icons8.com/color/144/000000/html-5--v2.png"/>
+    <img src="https://img.icons8.com/color/144/000000/nodejs.png"/>
+</div>
+
+## CONTACT INFO
+
+<a href="https://www.linkedin.com/in/amaroribeiro/" target="blank">
+    <img src="https://img.icons8.com/office/80/000000/linkedin.png"/>
+</a>
